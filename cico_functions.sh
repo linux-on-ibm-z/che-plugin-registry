33de0ee4de8f871de465d83ffe148d3cc454eeeb
aditi_jadhav <aditi.jadhav@ibm.com>
2020-06-03 17:23:42 +0530

    Added function to check buildx support

--- cico_functions.sh
+++ cico_functions.sh
@@ -46,39 +46,38 @@ function install_deps() {
     git
 
   service docker start
+    
+  #Enable qemu and binfmt support
+  docker run --rm --privileged docker/binfmt:66f9012c56a8316f9244ffd7622d7c21c1f6f28d
+  docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
   echo 'CICO: Dependencies installed'
-  
+}
+
+function check_buildx_support() {
   export DOCKER_BUILD_KIT=1
   export DOCKER_CLI_EXPERIMENTAL=enabled
-
+  
   docker_version="$(docker --version | cut -d' ' -f3 | tr -cd '0-9.')"
   if [[ "$(version "$docker_version")" < "$(version '19.03')" ]]; then
     echo "Docker $docker_version greater than or equal to 19.03 is required."
+    exit 1
   fi
-
-  docker_experimental="$(docker version | \
-                         awk '/^ *Experimental:/ {print $2 ; exit}')"
-  if [[ "$docker_experimental" != 'true' ]]; then
-    echo "Docker experimental flag not enabled:"\
-          "Set with 'export DOCKER_CLI_EXPERIMENTAL=enabled'"
-  else
-    echo "Docker $docker_version supports buildx experimental feature."
-  fi
-
+  
   # Kernel
   kernel_version="$(uname -r)"
   if [[ "$(version "$kernel_version")" < "$(version '4.8')" ]]; then
     echo "Kernel $kernel_version too old - need >= 4.8." \
           " Install a newer kernel."
-    exit 1
   else
     echo "kernel $kernel_version has binfmt_misc fix-binary (F) support."
   fi
-
-  #Enable qemu and binfmt support
-  docker run --rm --privileged docker/binfmt:66f9012c56a8316f9244ffd7622d7c21c1f6f28d
-  docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+  
 }
+
+function version() {
+  printf '%02d' $(echo "$1" | tr . ' ' | sed -e 's/ 0*/ /g') 2>/dev/null
+}
+
 
 function set_release_tag() {
   # Let's obtain the tag based on the 
@@ -129,10 +128,8 @@ function build_and_push() {
   fi
   
   # Create a new builder instance using buildx  
-  docker buildx create --name plugin_builder
-  docker buildx use plugin_builder
+  docker buildx create --use --name builder
   docker buildx inspect --bootstrap
-  docker buildx ls
 
   set_git_commit_tag
   
